72f90c724bae0a6b6fe62b95b96b3e2b3f9027cb
Uncpy <kajotek1@gmail.com>
2016-03-19 16:17:31 +0100

    repair

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback', to: "sessions#create"
 
-  delete 'sign_out', to: "sessions#destroy" #, as: 'sign_out'
+  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
 
   resources :events
   resources :invites
